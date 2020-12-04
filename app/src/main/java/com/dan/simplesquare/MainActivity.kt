96d1261cc529d30ad61ebe01749f41d8a0407a1c
Dan Oprea <dan.oprea@masternaut.com>
2020-12-04 11:12:18 +0100

    Ask permissions

--- app/src/main/java/com/dan/simplesquare/MainActivity.kt
+++ app/src/main/java/com/dan/simplesquare/MainActivity.kt
@@ -1,15 +1,70 @@
 package com.dan.simplesquare
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dan.simplesquare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val PERMISSIONS = arrayOf<String>(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        const val REQUEST_PERMISSIONS = 1
+    }
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!askPermissions())
+            onPermissionsAllowed()
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSIONS -> handleRequestPermissions(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun askPermissions(): Boolean {
+        for (permission in PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSIONS)
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private fun handleRequestPermissions(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        var allowedAll = grantResults.size >= PERMISSIONS.size
+
+        if (grantResults.size >= PERMISSIONS.size) {
+            for ( result in grantResults ) {
+                if (result != PackageManager.PERMISSION_GRANTED ) {
+                    allowedAll = false
+                    break
+                }
+            }
+        }
+
+        if( allowedAll ) {
+            onPermissionsAllowed()
+        } else {
+            setResult(0);
+            finish();
+        }
+    }
+
+    private fun onPermissionsAllowed() {
         binding = ActivityMainBinding.inflate( layoutInflater )
 
         setContentView(binding.root)
